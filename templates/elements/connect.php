69257e4330c6f1d28ae0782da654b8f8468edd31
Kevin From <kevin@kevinfrom.dk>
2025-08-12 19:29:42 +0200

    added styling for <code> tags

--- templates/elements/connect.php
+++ templates/elements/connect.php
@@ -12,7 +12,7 @@
             <p><a href="http://cdn.twinstar-wow.com/WoW_Vanilla.zip" download class="connect__link">Download the WoW client</a></p>
         </li>
         <li>
-            <p>Edit the <code class="connect__code">realmlist.wtf</code> file and change it to <code class="connect__code">set realmlist wow.lab.fromsoftware.dk</code>.</p>
+            <p>Edit the <code>realmlist.wtf</code> file and change it to <code>set realmlist wow.lab.fromsoftware.dk</code>.</p>
         </li>
         <li>
             <p>
@@ -20,7 +20,7 @@
                 <a href="https://legacy-wow.com/vanilla-addons" target="_blank" rel="noreferrer" class="connect__link">legacy-wow.com</a>
             </p>
         </li>
-        <li><p>Launch the game using the <code class="connect__code">wow.exe</code> file.</p></li>
+        <li><p>Launch the game using the <code>wow.exe</code> file.</p></li>
         <li><p>Sign in using your account and play!</p></li>
     </ol>
 </div>
